2acaf2188d752dc9e74931d972b912f461036201
zxj5470 <zxj5470@foxmail.com>
2019-06-01 16:59:08 +0800

    temp

--- sample.kt
+++ sample.kt
@@ -1,7 +1,6 @@
-a=2
+val a = 2
 // 4.8
-var `var`:String = "Spinn\"ing at the Boundary"
-
+var `var`: String = "Spinn\"ing at the Boundary"
 
 /*
 another = "It's Escaped"/*
@@ -9,10 +8,19 @@ another = "It's Escaped"/*
 3 * 2 + 1
 
 1 + 3 % 4*/*/
+val e = 1 + 2 + 3 + 4
+var bool = false
 
-x = 100
-y = "1"
-
-flt = 2.33f
-dbl = 4.66
+val emm = 1 + "emmmm" - 2333 / 2 + 2.0 + 233 is XYY
+val t = zxj is Philosophier
+val x = 100
+var y = "1"
+y = x+1
+var flt = 2.33f
+var dbl = 4.66
 //y = 1
+ice1000.xyy.tzxjcsbz()
+xyy.tzxjcsbz()
+xyy.tzxjcsbz(emmmm,huaQ)
+println(23333)
+csbz()()
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ extern crate pest_derive;
 //use self::AstNode::*;
 use pest::error::Error;
 use pest::Parser;
-use std::ffi::CString;
 
 #[derive(Parser)]
 #[grammar = "ik.pest"]
@@ -14,23 +13,38 @@ pub struct IKParser;
 
 #[derive(PartialEq, Debug, Clone)]
 pub enum AstNode {
-    Print(Box<AstNode>),
+    Boolean(bool),
     Int(i32),
-
     Float(f32),
     Double(f64),
+    Ident(String),
+    String(String),
+    FunctionCall {
+        caller: String,
+        ident: String,
+        expr_list: String,
+        ret: String,
+    },
     GlobalAssign {
         ident: String,
         expr: Box<AstNode>,
     },
     GlobalDecl {
         modifier: String,
-        type_str: String,
+        typ: String,
         ident: String,
         expr: Box<AstNode>,
     },
-    Ident(String),
-    Str(CString),
+    Infix {
+        a: Box<AstNode>,
+        op: String,
+        b: Box<AstNode>,
+    },
+    IsExpr {
+        a: Box<AstNode>,
+        b: Box<AstNode>,
+    },
+    None,
 }
 
 pub fn parse(source: &str) -> Result<Vec<AstNode>, Error<Rule>> {
@@ -39,10 +53,13 @@ pub fn parse(source: &str) -> Result<Vec<AstNode>, Error<Rule>> {
     let pairs = IKParser::parse(Rule::program, source)?;
     for pair in pairs {
         match pair.as_rule() {
-            Rule::expr => {
+            Rule::assgmtExpr => {
                 ast.push(build_ast_from_expr(pair));
             }
             Rule::declStmt => {
+                ast.push(build_ast_from_expr(pair));
+            }
+            Rule::functionCall => {
                 ast.push(build_ast_from_expr(pair));
             }
             _ => {}
@@ -76,6 +93,19 @@ fn build_ast_from_literal(pair: pest::iterators::Pair<Rule>) -> AstNode {
                 _ => AstNode::Double(dstr.parse().unwrap())
             }
         }
+        Rule::boolean => {
+            let dstr = pair.as_str();
+            let dstr = &dstr[..];
+            let value: bool = dstr.parse().unwrap();
+            AstNode::Boolean(value)
+        }
+        Rule::string => {
+            let str = &pair.as_str();
+            let str = &str[1..str.len() - 1];
+            // Escaped string quotes become single quotes here.
+            let str = str.replace("''", "'");
+            AstNode::String(String::from(&str[..]))
+        }
         unknown_term => panic!("Unexpected term: {:?}", unknown_term),
     }
 }
@@ -83,16 +113,20 @@ fn build_ast_from_literal(pair: pest::iterators::Pair<Rule>) -> AstNode {
 
 fn build_ast_from_expr(pair: pest::iterators::Pair<Rule>) -> AstNode {
     match pair.as_rule() {
-        Rule::expr => build_ast_from_expr(pair.into_inner().next().unwrap()),
+        Rule::decimal
+        | Rule::integer
+        | Rule::infixChar
+        | Rule::boolean
+        | Rule::string => build_ast_from_literal(pair),
         Rule::ident => AstNode::Ident(String::from(pair.as_str())),
-        Rule::decimal => build_ast_from_literal(pair),
+
         Rule::declStmt => {
             let mut pair = pair.into_inner();
             let modifier = pair.next().unwrap().as_str();// var val
             let mut ident = pair.next().unwrap().as_str();
             let third_expr = pair.next().unwrap();// :Type or expr after `=`
-            let mut type_str = "";
-            let mut expr;
+            let mut type_str;
+            let expr;
 
             let str = ident;
             if &str[0..1] == "`" && &str[str.len() - 1..str.len()] == "`" {
@@ -100,21 +134,22 @@ fn build_ast_from_expr(pair: pest::iterators::Pair<Rule>) -> AstNode {
             }
 
             if third_expr.as_rule() == Rule::typeNotation {
-                type_str = third_expr.as_str();
-                type_str = &type_str[1..type_str.len()];
+                let str = third_expr.as_str();
+                let str = &str[1..str.len()];
+                type_str = String::from(str);
                 expr = build_ast_from_expr(pair.next().unwrap());
             } else {
                 expr = build_ast_from_expr(third_expr);
+                type_str = get_type_from(&expr);
             }
-
             AstNode::GlobalDecl {
                 modifier: String::from(modifier),
-                type_str: String::from(type_str),
+                typ: type_str,
                 ident: String::from(ident),
                 expr: Box::new(expr),
             }
         }
-        Rule::integer => build_ast_from_literal(pair),
+
         Rule::assgmtExpr => {
             let mut pair = pair.into_inner();
             let ident = pair.next().unwrap();
@@ -125,15 +160,85 @@ fn build_ast_from_expr(pair: pest::iterators::Pair<Rule>) -> AstNode {
                 expr: Box::new(expr),
             }
         }
-        Rule::string => {
-            let str = &pair.as_str();
-            // Strip leading and ending quotes.
-            let str = &str[1..str.len() - 1];
-            // Escaped string quotes become single quotes here.
-            let str = str.replace("''", "'");
-            AstNode::Str(CString::new(&str[..]).unwrap())
+        Rule::functionCall => {
+            let mut pair = pair.into_inner();
+            let ident = pair.next().unwrap();
+            let mut caller = "";
+            let id = match ident.as_rule() {
+                Rule::functionCaller => {
+                    caller = ident.as_str().trim_end_matches(".");
+                    pair.next().unwrap().as_str()
+                }
+                _ => ident.as_str()
+            };
+            let expr_list = pair.next();
+            let list = match expr_list {
+                None => "none",
+                _ => expr_list.unwrap().as_str()
+            };
+            AstNode::FunctionCall {
+                caller: String::from(caller),
+                ident: String::from(id),
+                expr_list: String::from(list),
+                ret: String::from("Unit"),
+            }
         }
+
+        Rule::isExpr => build_ast_is_expr(pair),
+        Rule::infixExpr => build_ast_expr(pair),
+        Rule::expr => build_ast_expr(pair),
         unknown_expr => panic!("Unexpected expression: {:?}", unknown_expr),
+    }
+}
+
+fn get_type_from(ast: &AstNode) -> String {
+    match ast {
+        AstNode::IsExpr { a: _, b: _ } => String::from("Boolean"),
+        AstNode::Infix { a: _, op: _, b: _ } => String::from("<unknown>"),
+        _ => {
+            let str = format!("{:?}", ast.to_owned());
+            let len = str.len();
+            let size = str.find("(");
+            let ret = match size {
+                None => len,
+                _ => size.unwrap()
+            };
+            String::from(&str[..ret])
+        }
+    }
+}
+
+fn build_ast_is_expr(pair: pest::iterators::Pair<Rule>) -> AstNode {
+    let mut inner_par = pair.into_inner();
+    let a = inner_par.next().unwrap();
+    let a = build_ast_from_expr(a);
+
+    let b = inner_par.next().unwrap();
+    let b = build_ast_from_expr(b);
+
+    AstNode::IsExpr { a: Box::new(a), b: Box::new(b) }
+}
+
+fn build_ast_expr(pair: pest::iterators::Pair<Rule>) -> AstNode {
+    let mut inner_par = pair.into_inner();
+    let a = inner_par.next();
+    if a == None {
+        AstNode::None
+    } else {
+        let expr = build_ast_from_expr(a.unwrap());
+        let op = inner_par.next();
+        if op == None {
+            expr
+        } else {
+            let opstr = op.unwrap().as_str();
+            let b = inner_par.next().unwrap();
+            let expr_b = build_ast_from_expr(b);
+            AstNode::Infix {
+                a: Box::new(expr),
+                op: String::from(opstr),
+                b: Box::new(expr_b),
+            }
+        }
     }
 }
 
